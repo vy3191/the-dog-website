c8aaa59ebf6f15e3d792a9f32bd8a88ccfd51c80
vy3191 <venkyafhs@gmail.com>
2019-10-07 20:26:46 -0400

    Changed class component to functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,49 +1,28 @@
 import "./App.css";
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class App extends Component {
-  constructor() {
-    super()
-
+function App()  {
+  
     // set some initial/default state values
-    this.state = {
-      breed: 'husky',
-      images: []
-    }
-  }
+    const [breed, setBreed] = useState('husky');
+    const [images, setImages] = useState([]);
+      
+    useEffect( () => {
+      // fetch images for the default state value (husky)
+       setImages([]);
+       fetchDogImages();
+    },[breed])  
+    
+  
 
-  componentDidMount() {
-    // fetch images for the default state value (husky)
-    this.fetchDogImages()
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // this if statement prevents an infinite loop
-    // by only re-fetching if the breed has changed since the last update
-    //
-    // ----- same idea -----
-    //   useEffect(() => {
-    // 
-    //   }, [breed])
-    //
-    if (prevState.breed !== this.state.breed) {
-      // give the user feedback that something is happening
-      // by clearing out the current images while fetching new ones
-      this.setState({ images: [] })
-
-      // re-fetch images with new state value
-      this.fetchDogImages()
-    }
-  }
+  
 
   // extract this function from lifecycle event since it's used multiple times
-  fetchDogImages = () => {
-    axios.get(`https://dog.ceo/api/breed/${this.state.breed}/images`)
-      .then(result => {
-        this.setState({
-          images: result.data.message
-        })
+  const fetchDogImages = () => {
+    axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+      .then(result => {        
+        setImages(result.data.message);
       })
       .catch(error => {
         console.log('error:', error)
@@ -51,18 +30,15 @@ class App extends Component {
   }
 
   // make our select field controlled by react state
-  handleChange = (event) => {
-    this.setState({
-      breed: event.target.value
-    })
+  const handleChange = (event) => {
+      setBreed(event.target.value);
   }
-
-  render() {
-    return (
+ 
+  return (
       <>
         <h1>The Dog Website</h1>
 
-        <select value={this.state.breed} onChange={this.handleChange}>
+        <select value={breed} onChange={handleChange}>
           <option value="husky">Husky</option>
           <option value="beagle">Beagle</option>
           <option value="corgi">Corgi</option>
@@ -70,13 +46,13 @@ class App extends Component {
         </select>
 
         <div>
-          {this.state.images.map((image, index) => (
+          {images.map((image, index) => (
             <img key={index} src={image} alt="Dog" />
           ))}
         </div>
       </>
     )
   }
-}
+
 
 export default App;
